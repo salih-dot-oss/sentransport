702404c082d798d722f38715450c90af95e6e5c6
SammNet <contact.sammnet@proton.me>
2026-04-28 16:55:01 +0000

    Lab 2 : composants statiques LigneBus et ListeLignes

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      &copy; 2026 SenTransport &mdash; ESP/UCAD
+    </footer>
+  );
+}
+
+export default Footer;
--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,21 @@
 import './Header.css';
 function Header () {
+    const date = new Date().toLocaleDateString('fr-FR', {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+    });
     return (
         <header className ="header">
         <h1 className ="header-titre">SenTransport</h1>
         <p className ="header-soustitre">
             Votre guide du transport en commun a Dakar
         </p >
+        <p className="header-date"> {date} </p>
         </header>
+        
     );
+    
 }
 export default Header ;
